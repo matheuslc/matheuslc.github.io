e880a38977aa0b4db3053fd61017f6e20b65d3e0
Matheus Carmo <me@matheuslc.com>
2014-06-05 23:27:37 -0300

    Add a random function

--- js/main.js
+++ js/main.js
@@ -2,17 +2,52 @@
 	'use strict';
   		
   		var btn,
-  			change;
+  			  change,
+          items;
+
+        items = [
+           {
+              bg: '#2980b9',
+              img: 'matheuslcflatsmall.jpg'
+           },
+
+           {
+              bg: 'url(../img/beach.jpg)',
+              img: 'matheuslcbeachsmall.jpg'
+           }
+        ];
+
+       var randomize = function (firstvalue, secondvalue) {
+                var min = 0,
+                    max = 0;
+
+                // Check min a max value
+                if (firstvalue > secondvalue) {
+                    max = firstvalue;
+                    min = secondvalue;
+                } else {
+                    max = secondvalue;
+                    min = firstvalue;
+                };
+
+                // Check if it's a decimal or integer number
+                if ((max % 1 === 0) && (min % 1 === 0)) {
+                    return Math.floor(Math.random()*(max-min+1)+min);
+                } else {
+                    return (Math.random() * (max-min)+min).toFixed(2);
+                };
+};
 
 
   		change = function() {
   			var img,
-  				bg,
-  				i,
-  				sheet,
-  				css;
+  				  bg,
+  				  i,
+            n,
+  				  sheet,
+  				  css;
 
-  			css = document.styleSheets;
+  			css = document.querySelector('body');
   			img = document.querySelector(".photo img");
 
   			// Catch the element
@@ -22,10 +57,11 @@
   				}
   			}
 
-  			sheet.insertRule("body { background: url('../img/beach.jpg') !important; background-size: cover !important;}", 1);
+        n = randomize(0, items.length - 1);
 
-  			img.setAttribute('src', "img/matheuslcbeachsmall.jpg");
-
+        css.style.background = items[n].bg;
+        css.style.backgroundSize = "cover";
+        img.setAttribute('src', "img/" + items[n].img);
 
   		};
 
