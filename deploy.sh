16e3cd46b1f34261b0a66a6d4d694821e971ae75
Matheus Carmo <mematheuslc@gmail.com>
2020-08-20 00:16:00 -0300

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # If a command fails then the deploy stops
 set -e
@@ -8,18 +8,16 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
 ../hugo/./hugo -t 'monopriv'
 
-# Go To Public folder
-cd public
+git branch -D gh-pages
+git checkout -b gh-pages
 
-# Add changes to git.
+shopt -s extglob
+rm -r !(public|CNAME)
+
+mv public/* .
 git add .
+git commit -m "Remove everything except public package"
+git push origin gh-pages
 
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
 
-# Push source and build repos.
-git push origin master
+
